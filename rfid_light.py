2f2c198b2b5df91b644fad97020de423220f7b01
goliatone <burgosemi@gmail.com>
2015-03-13 20:01:21 -0400

    clean up, sending request

--- rfid_light.py
+++ rfid_light.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sys
 import time
+import httplib
+import requests
+import json
 from evdev import InputDevice, list_devices, categorize, ecodes
 
 SCANCODES = {
@@ -14,46 +18,85 @@ SCANCODES = {
     50: u'M', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
 }
 
+KEY_ENTER = 'KEY_ENTER'
+DEVICE_NAME = 'RFIDeas USB Keyboard'
+URI = '/lights/read'
+HOST = '192.168.1.145'
+PORT = '3000'
+URL_TEMPLATE = 'http://%s:%s%s'
+UUID = '34add6809dd36514dd43811455cfb596'
+
+
+def get_default_payload(rfid):
+    return {'uuid': UUID, 'rfid': rfid}
+
+
+def make_request(rfid):
+    payload = get_default_payload(rfid)
+
+    url = URL_TEMPLATE % (HOST, PORT, URI)
+    headers = {'content-type': 'application/json'}
+
+    try:
+        requests.post(url, data=json.dumps(payload), headers=headers)
+    except Exception as e:
+        print 'Error: ', e
+
+
 
 def get_scanner_device():
     devices = map(InputDevice, list_devices())
     device = None
     for dev in devices:
-        print dev.name
-    if dev.name == 'RFIDeas USB Keyboard':
-        device = InputDevice(dev.fn)
+        if dev.name == DEVICE_NAME:
+            device = dev
             break
     return device
 
 
-def init(device):
-    pass
-
-
-if __name__ == "__main__":
-    device = get_scanner_device()
-    if str(device) == 'None':
-    print "Device not found"
-    sys.exit(1)
-
-    try:
-    device.grab()
-    except:
-        print "Unable to grab input device"
-    sys.exit(1)
-
-    init(device)
-
-    while True:
-    try:
-        rfid = ""
-        for event in device.read_loop():
+def read_input(device):
+    rfid = ''
+    for event in device.read_loop():
         data = categorize(event)
         if event.type == ecodes.EV_KEY and data.keystate == data.key_down:
-            if data.keycode == 'KEY_ENTER':
-            break
+            if data.keycode == KEY_ENTER:
+                break
             rfid += SCANCODES[data.scancode]
-        print "KEY CODE %s" %rfid
-    except ValueError:
-        print "ERROR"
-        time.sleep(0.1)
+    return rfid
+
+
+def init(dev):
+    if str(dev) == 'None':
+        print "Device not found"
+        sys.exit(1)
+
+    try:
+        device = InputDevice(dev.fn)
+        device.grab()
+    except:
+        print "Unable to grab input device"
+        sys.exit(1)
+
+    return device
+
+
+def cleanup(device):
+    device.ungrab()
+
+if __name__ == "__main__":
+    device_name = get_scanner_device()
+    device = init(device_name)
+
+    print "Found device: %s" % DEVICE_NAME
+
+    while True:
+        try:
+            rfid = read_input(device)
+            make_request(rfid)
+            print "RFID card read, value: %s" %rfid
+        except ValueError:
+            time.sleep(0.1)
+        except Exception, e:
+            print e
+            cleanup(device)
+            sys.exit(1)
